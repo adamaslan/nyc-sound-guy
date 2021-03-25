0945829f049181dd932a7583c91c11dd40327c85
Adam Aslan <chillcoders@gmail.com>
2021-03-24 22:47:04 -0400

    carosel

---
+++ src/Caro.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 250px;
+  width: 100%;
+  object-fit: fill;
+  background-color: transparent;
+  color: palevioletred;
+  margin: 15px;
+  font-size: 4em;
+`;
---
+++ src/Emph.js
@@ -0,0 +1,11 @@
+// Emph.js;
+
+import styled from "styled-components";
+
+export default styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 10px solid palevioletred;
+  border-radius: 10px;
+`;
