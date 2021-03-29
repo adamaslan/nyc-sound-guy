2261fef9d17fd7326122bdf07d7d0b92b3fe1703
Adam Aslan <chillcoders@gmail.com>
2021-03-29 16:29:35 -0400

    styling

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,27 +1,32 @@
 import React from "react";
-
-import AreaCode from "../Assets/843.jpg";
-
+import Button from "../Button";
+import Emph2 from "../Emph2";
 
 export default function About() {
   return (
     <div>
-      <p>
-        NYC SOUND GUY is a service run by Adam Aslan, a location sound mixer
-        based in NYC. Working as a sound mixer/recordist in film for the past 9
-        years, I am able to spot some of the most talented sound mixers out
-        there.
-        <br /> I am available for hire when I am not working for my regular
-        clients and am in direct contact with all the sound mixers on this page
-        as to their availability and rates. Feel free to contact me with any nyc
-        sound mixer needs you may have. Even if all the sound mixers I have
-        listed are busy there are a ton of other New York City sound mixers I
-        know that I can confirm are available at a rate fair to any type of
+      <Emph2>
+        NYC SOUND GUY is a service that connects productions to many of the most
+        talented sound mixers out there.
+        <br />
+        <br /> We are in direct contact with all the sound mixers on this page
+        as to their availability and rates. Feel free to contact us with any nyc
+        sound mixer needs you may have. Even if all the sound mixers we have
+        listed are busy there are a ton of other New York City sound mixers we
+        know that we can confirm are available at a rate fair to any type of
         production. <br />
-        Contact us at nycsoundpeople@gmail.com or at{" "}
-        <img src={AreaCode} alt="area code" />
-        -284-3462
-      </p>
+        <br />
+        Contact us today!
+        <h3>
+          <Button onClick={() => alert("8432843462")}>
+            Click here to get our number
+          </Button>{" "}
+          <br />
+          <Button onClick={() => alert("nycsoundpeople@gmail.com")}>
+            Click here to get our email
+          </Button>{" "}
+        </h3>
+      </Emph2>
     </div>
   );
 }
