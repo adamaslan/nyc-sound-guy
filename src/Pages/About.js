ac274dd84e8c8e9e22f14223075a3a04a3b9c59a
Adam Timur Aslan <adamtimuraslan@gmail.com>
2022-08-18 10:56:26 -0400

    resources

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -9,13 +9,15 @@ export default function About() {
         NYC SOUND GUY is a service that connects productions to many of the most
         talented sound mixers out there.
         <br />
-        <br /> We are in direct contact with all the sound mixers on this page
+        <br /> <p>We are in direct contact with all the sound mixers on this page
         as to their availability and rates. Feel free to contact us with any nyc
         sound mixer needs you may have. Even if all the sound mixers we have
         listed are busy there are a ton of other New York City sound mixers we
         know that we can confirm are available at a rate fair to any type of
-        production. <br />
-        <br />
+        production.</p> <br />
+        
+        We are able to travel to any location in the world and have the travel gear necessary to expedite shoots associated with lots of travel.
+        <br /> <br />
         Contact us today!
         <h3>
           <Button onClick={() => alert("8432843462")}>
